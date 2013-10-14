13088b320176c378966226d58b860ab27c7d0100
Pliman <sunbin@nhn.com>
2013-10-14 13:30:56 +0800

    init

---
+++ jars/velocity-tools(must add)/添加到自定义库.sql
@@ -0,0 +1,21 @@
+mvn install:install-file -Dfile=antlr-2.7.2.jar -DgroupId=net.pliman -DartifactId=antlr -Dversion=2.7.2 -Dpackaging=jar
+
+mvn install:install-file -Dfile=commons-chain-1.1.jar -DgroupId=net.pliman -DartifactId=commons-chain -Dversion=1.1 -Dpackaging=jar
+
+mvn install:install-file -Dfile=commons-digester-1.8.jar -DgroupId=net.pliman -DartifactId=commons-digester -Dversion=1.8 -Dpackaging=jar
+
+mvn install:install-file -Dfile=commons-validator-1.3.1.jar -DgroupId=net.pliman -DartifactId=commons-validator -Dversion=1.3.1 -Dpackaging=jar
+
+mvn install:install-file -Dfile=junit-4.8.2.jar -DgroupId=net.pliman -DartifactId=junit -Dversion=4.8.2 -Dpackaging=jar
+
+mvn install:install-file -Dfile=oro-2.0.8.jar -DgroupId=net.pliman -DartifactId=oro -Dversion=2.0.8 -Dpackaging=jar
+
+mvn install:install-file -Dfile=sslext-1.2-0.jar -DgroupId=net.pliman -DartifactId=sslext -Dversion=1.2.0 -Dpackaging=jar
+
+mvn install:install-file -Dfile=struts-core-1.3.8.jar -DgroupId=net.pliman -DartifactId=struts-core -Dversion=1.3.8 -Dpackaging=jar
+
+mvn install:install-file -Dfile=struts-taglib-1.3.8.jar -DgroupId=net.pliman -DartifactId=struts-taglib -Dversion=1.3.8 -Dpackaging=jar
+
+mvn install:install-file -Dfile=struts-tiles-1.3.8.jar -DgroupId=net.pliman -DartifactId=struts-tiles -Dversion=1.3.8 -Dpackaging=jar
+
+mvn install:install-file -Dfile=velocity-tools-2.0.jar -DgroupId=net.pliman -DartifactId=velocity-tools -Dversion=2.0 -Dpackaging=jar
